45c2e1fc5554e1083084789a0237d7ede5cf09b2
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-05-29 16:44:39 +1000

    building login page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Login.css'
+
+function Login () {
+    return (
+        <div className="login-page">
+            <h2>LOGIN</h2>
+            <p>Please login to your account to enjoy member-only benefits.</p>
+            <form id="login-form">
+                <label for="username"><strong>Username</strong></label>
+                <input className="fill-input" type="text" id="username" name="username" required /> 
+                <a href="">Forgot username</a>
+                <label for='password'><strong>Password</strong></label>
+                <input className="fill-input" type="password" id="password" name="password" required />
+                <div id="show-password-container">
+                    <input type="checkbox" name="show-password" id="show-password" />
+                    <label for="show-password">Show password</label>
+                </div>
+                <a href="">Forgot password</a>
+                <button type="submit">Login</button>                  
+            </form>
+            <p className="join-suggestion">
+                Don't have an account?
+                <a href="">  Join today</a>
+            </p>
+        </div>
+    )
+}
+
+export default Login
