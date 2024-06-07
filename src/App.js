3ecdcfc58639fbb46f70971a3b6b3df5a4f5ddd2
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-06-07 22:49:11 +1000

    modified the files to start new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./components/Login/Login";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
