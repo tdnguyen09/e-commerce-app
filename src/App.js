5be0c14c8a68854090939c3d6015a6d3eb2516b5
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-07-15 23:42:59 +1000

    add component and context

--- src/App.js
+++ src/App.js
@@ -8,31 +8,49 @@ import Signup from "./components/Signup/Signup";
 import RefundPolicy from "./components/Policies/Refund-policy";
 import TermOfService from "./components/Policies/TermofService";
 import PrivacyPolicy from "./components/Policies/Privacy-policy";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import ProductList from "./components/ProductList/ProductList";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import { Provider } from "./components/WebContext";
 
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path='/refund-policy'>
-          <RefundPolicy />
-        </Route>
-        <Route path="/term-of-service">
-          <TermOfService />
-        </Route>
-        <Route path="/privacy-policy">
-          <PrivacyPolicy />
-        </Route>
-      </Switch>
-      <Footer />
+      <Provider>
+        <NavBar />
+        <Switch>
+          <Route path='/shopping-cart'>
+            <ShoppingCart />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path='/refund-policy'>
+            <RefundPolicy />
+          </Route>
+          <Route path="/term-of-service">
+            <TermOfService />
+          </Route>
+          <Route path="/privacy-policy">
+            <PrivacyPolicy />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails />
+          </Route>
+          <Route exact path="/products">
+            <ProductList />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Provider>
     </div>
   );
 }
