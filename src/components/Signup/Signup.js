0bb6daf49f397aa50d85407d4557e1d8a711b5d8
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-08-16 01:19:30 +1000

    modified some name to able signup new member

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,44 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css"
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function Signup() {
+    const [formData, setFormData] = useState({
+        firstname: "",
+        lastname:"",
+        email:"",
+        password:"",
+        phone:"",
+        address:"",
+        suburb:"",
+        state:"",
+        postcode:""
+    })
+    function handleChange(event) {
+        let name = event.target.name
+        let value = event.target.value
+
+        setFormData ({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        const userData = {
+            firstname: formData.firstname,
+            lastname: formData.lastname,
+            email: formData.email,
+            password: formData.password,
+            address: formData.address,
+            state: formData.state,
+            postcode: formData.postcode,
+            suburb: formData.suburb,
+            phone: formData.phone
+        };
+        fetch("http://127.0.0.1:5000/signup", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        })
+            .then( res => res.json())
+            .then(userData => console.log(userData))
+        //set inputs back to empty
+        setFormData({
+            firstname: "",
+            lastname:"",
+            email:"",
+            password:"",
+            phone:"",
+            address:"",
+            suburb:"",
+            state:"",
+            postcode:""
+        })
+    }
     return (
         <div className="signup-page">   
             <h1>Register</h1>
             <h4>Please fill in the fields below:</h4>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input-container">
-                    <input type="text" id="firstname-input" className="input-field" required />
+                    <input type="text" id="firstname-input" className="input-field" name="firstname" onChange={handleChange} value={formData.firstname} required />
                     <label for="firstname-input" className="input-label">Firstname</label>
                 </div>
                 <div className="input-container">
-                    <input type="text" id="lastname-input" className="input-field" required />
+                    <input type="text" id="lastname-input" className="input-field" name='lastname' onChange={handleChange} value={formData.lastname} required />
                     <label for="lastname-input" className="input-label">Lastname</label>
                 </div>
                 <div className="input-container">
-                    <input type="email" id="email-input-register" className="input-field" required />
+                    <input type="email" id="email-input-register" className="input-field" name="email" onChange={handleChange} value={formData.email} required />
                     <label for="email-input-register" className="input-label">Email</label>
                 </div>
                 <div className="input-container">
-                    <input type="password" id="password-input-register" className="input-field" required />
+                    <input type="password" id="password-input-register" className="input-field" name="password" onChange={handleChange} value={formData.password} required />
                     <label for="password-input-register" className="input-label">Password</label>
                 </div>
-                <div className="input-container">
-                    <input type="password" id="password-input-confirm" className="input-field" required />
+                <div className="input-container"> 
+                    <input type="password" id="password-input-confirm" className="input-field" name="password-confirm" required />
                     <label for="password-input-confirm" className="input-label">Confirm Password</label>
                 </div>
                 <div className="input-container">
-                    <input type="text" id="phone-input" className="input-field" required />
+                    <input type="text" id="phone-input" className="input-field" name="phone" onChange={handleChange} value={formData.phone} required />
                     <label for="phone-input" className="input-label">Phone</label>
                 </div>
                 <div className="input-container">
-                    <input type="text" id="address-input" className="input-field" required />
+                    <input type="text" id="address-input" className="input-field" name="address" onChange={handleChange} value={formData.address} required />
                     <label for="address-input" className="input-label">Address</label>
                 </div>
                 <div id="address-additional-input">
-                    <input type="text" id="suburb-input" placeholder="Suburb/City" className="address-input-field" required />
-                    <select id="state-register" name="state-register" className="address-input-field">
+                    <input type="text" id="suburb-input" placeholder="Suburb/City" name="suburb" className="address-input-field" onChange={handleChange} value={formData.suburb} required />
+                    <select id="state-register" name="state" className="address-input-field" onChange={handleChange} value={formData.state}>
                         <option value="ACT">ACT</option>
                         <option value="NSW">NSW</option>
                         <option value="NT">NT</option>
@@ -48,7 +104,7 @@ function Signup() {
                         <option value="VIC">VIC</option>
                         <option value="WA">WA</option>
                     </select>
-                    <input type="text" id="postcode-input" placeholder="Postcode" className="address-input-field" required />
+                    <input type="text" id="postcode-input" placeholder="Postcode" className="address-input-field" name="postcode" onChange={handleChange} value={formData.postcode} required />
                 </div>
                 <button id="btn-register" type="submit">CREATE ACCOUNT</button>
             </form>
