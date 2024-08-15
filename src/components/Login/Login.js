cb0cd15673ee35bcc46d61bfd2d5acbd1a8c63d3
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-08-16 01:18:41 +1000

    add function for handle submit login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css'
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-function Login () {
+
+
+function Login ({ setUser }) {
+    const [passwordVisible, setPasswordVisible] = useState(false)
+    const history = useHistory()
+    const [formLogin, setFromLogin] = useState({
+        email:"",
+        password:""
+    });
+
+    function handleChange (e) {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        setFromLogin({
+            ...formLogin,
+            [name]: value,
+        })
+    }
+    function handleSubmit (e) {
+        e.preventDefault()
+        const loginData = {
+            email: formLogin.email,
+            password: formLogin.password
+        }
+
+        fetch("http://127.0.0.1:5000/login", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        })
+        .then(res => {
+            if (res.status === 200) {
+                console.log('login successfully')
+                history.push('/logout')
+            }
+            return res.json()
+        })
+        .then(loginData => {
+            setUser(loginData)
+            console.log(loginData)})
+        
+        setFromLogin({
+            email:"",
+            password:""
+        })
+    }
+
+    function showPassword () {
+        setPasswordVisible((prevPasswordVisible) => !prevPasswordVisible)
+    }
+
     return (
         <div className="login-page">
             <h2>LOGIN</h2>
             <p>Please login to your account to enjoy member-only benefits.</p>
-            <form id="login-form">
+            <form id="login-form" onSubmit={handleSubmit}>
                 <label for="username"><strong>Username</strong></label>
-                <input className="fill-input" type="text" id="username" name="username" required /> 
+                <input className="fill-input" type="text" id="login-email" name="email" onChange={handleChange} value={formLogin.email} required /> 
                 <a href="">Forgot username</a>
                 <label for='password'><strong>Password</strong></label>
-                <input className="fill-input" type="password" id="password" name="password" required />
+                <input className="fill-input" type={passwordVisible ? "text" : "password"} id="password" name="password" onChange={handleChange} value={formLogin.password} required />
                 <div id="show-password-container">
-                    <input type="checkbox" name="show-password" id="show-password" />
+                    <input type="checkbox" name="show-password" id="show-password" onChange={showPassword} checked={passwordVisible}/>
                     <label for="show-password">Show password</label>
                 </div>
                 <a href="">Forgot password</a>
