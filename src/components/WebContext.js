6fd2e5206ffdecc67550fd02ac5e0a3a60a3af7a
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-08-22 00:00:24 +1000

    deleting wishlistItems functions

--- src/components/WebContext.js
+++ src/components/WebContext.js
@@ -7,17 +7,15 @@ function Provider({ children }){
     const [isLogin, setIsLogin] = useState(false);
     const [username, setUsername] = useState('')
     const [cartItems, setCartItems] = useState([]);
-    const [wishlistItems, setWishlistItems] = useState([])
 
 //fetching data
     useEffect(() => {
-        fetch("https://json-server-31ga.onrender.com/toys")
+        fetch("http://127.0.0.1:5000/products")
         .then(res => res.json())
         .then(products => {
             setDatas(products)
         })
     }, [])
-
 //functions for carts
     function getItemQuantity(index) {
         const item = cartItems.find(item => item.id === index)
@@ -78,40 +76,12 @@ function Provider({ children }){
         setCartItems([])
     }
 
-//functions for wishlist
-
-    function getWishlishItemQuanity (index) {
-        const item = wishlistItems.find(item => item.id === index)
-        return item ? item.quantity : 0 ;
-    }
-
-    function addToWishlist(index) {
-        let quantity = getWishlishItemQuanity(index)
-        let itemWishlist = datas.find(data => data.id === index)
-        if (quantity === 0) {
-            setWishlistItems([
-                ...wishlistItems,
-                {
-                    id: index, 
-                    price:itemWishlist.price,
-                    name: itemWishlist.name,
-                    image: itemWishlist.image,
-                    quantity: 1
-                }
-            ])
-        } else {
-            return itemWishlist
-        }
-    }
-
-    function removeWishlistItem (index) {
-        let updatedWihslistItems = wishlistItems.filter(item => item.id !== index);
-        setWishlistItems(updatedWihslistItems)
-    }
-
-    function totalWishlishItemsQuantity () {
-        let totalWishlistQuantity = wishlistItems.reduce((total, item) => total + item.quantity, 0)
-        return totalWishlistQuantity
+    function totalCost () {
+        let total = 0;
+        cartItems.map(item => {
+            total += (item.price * item.quantity)
+        })
+        return total.toFixed(2)
     }
 
 // functions for login and logout
@@ -139,12 +109,9 @@ function Provider({ children }){
         removeOneFromCart,
         totalQuantity,
         deleteCart,
-        getWishlishItemQuanity,
-        addToWishlist,
-        removeWishlistItem,
-        totalWishlishItemsQuantity,
         login,
-        logout
+        logout, 
+        totalCost
     }
 
     return (
