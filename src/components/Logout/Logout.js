9d75a00c3e77745a00b020872a205fb0e966ec97
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-08-21 23:58:29 +1000

    testing logout request at backend

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+function Logout ({ setUser, user }) {
+    const history = useHistory()
+    const [userUpdate, setUserUpdate] = useState({});
+
+    function handleChange(e){
+        let name = e.target.name
+        let value = e.target.value
+        // setUserUpdate({
+        //     ...userUpdate,
+        //     [name]: value
+        // })
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
+
+    function handleUpdate(){
+        fetch('http://127.0.0.1:5000/update-detail',{
+            method:'PATCH',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => {
+            if(res.ok) {
+                res.json()
+            }else{
+                console.log(res.message)
+            }
+        })
+        .then(res => console.log(res))
+    }
+
+    function handleLogout() {
+        fetch('http://127.0.0.1:5000/logout', {
+            method: 'DELETE',
+        }).then (res =>{
+            if(res.status === 204){
+                setUser(null)
+                history.push('/login')
+                // return res.json()
+            } else {
+                console.error('failed to log out')
+            }
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error))
+    }
+    return (
+        <div>
+            <h2>Welcome {user.firstname} {user.lastname} </h2>
+            <div className="update-details">   
+                <div className="update-user-container">
+                    <label for='update-firstname'>Fristname</label>
+                    <input type="text" id="update-firstname" name="firstname" className="update-user-field" defaultValue={user.firstname} onChange={handleChange} value={user.firstname} />
+                </div>
+                <div className="update-user-container">
+                    <label for='update-lastname'>Lastname</label>
+                    <input type="text" id="update-lastname" name="lastname" className="update-user-field" defaultValue={user.lastname} onChange={handleChange} value={user.lastname} />
+                </div>
+                <div className="update-user-container">
+                    <label for='update-email'>Email</label>
+                    <input type="text" id="update-email" name="email" className="update-user-field" defaultValue={user.email} onChange={handleChange} value={user.email} />
+                </div>
+                <div className="update-user-container">
+                    <label for='update-phone'>Phone</label>
+                    <input type="text" id="update-phone" name="phone" className="update-user-field" defaultValue={user.phone} onChange={handleChange} value={user.phone} />
+                </div>
+                <div className="update-user-container">
+                    <label for='update-address'>Address</label>
+                    <input type="text" id="update-address" name="address" className="update-user-field" defaultValue={user.address} onChange={handleChange} value={user.address} />
+                </div>
+                <div className="update-user-container">
+                    <label for='update-suburd'>Suburb/City</label>
+                    <input type="text" id="update-suburb" name="suburb" defaultValue={user.suburb} onChange={handleChange} value={user.suburb} />
+                    <select id="update-state" name="state" defaultValue={user.state} onChange={handleChange} value={user.state}>
+                        <option value='ACT'>ACT</option>
+                        <option value='NSW'>NSW</option>
+                        <option value='NT'>NT</option>
+                        <option value='QLD'>QLD</option>
+                        <option value='SA'>SA</option>
+                        <option value='TAS'>TAS</option>
+                        <option value='VIC'>VIC</option>
+                        <option value='WA'>WA</option>
+                    </select>
+                    <input type="text" id="update-postcode" name="postcode" defaultValue={user.postcode} onChange={handleChange} value={user.postcode} placeholder="postcode" />
+                </div>
+                <button onClick={() => handleUpdate()}>Update</button>
+            </div>
+            <button onClick={handleLogout}>Logout</button> 
+        </div>
+    )
+}
+
+export default Logout;
