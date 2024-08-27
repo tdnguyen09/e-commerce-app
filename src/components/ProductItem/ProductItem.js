22580eddff43086d2d625aefb2c7fe9b573f2515
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-08-27 18:48:30 +1000

    putting some styles for div and p elements

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 
-function ProductItem({ image, name, price, clearance, id }){
+function ProductItem({ image, name, price, clearance, onsale, id, discount }){
     return (
-            <Link to={`/products/${id}`}>
+            <Link to={`/products/${id}`}  style={{ textDecoration: 'none' }}>
                 <img src={image} alt="product-name" />
                 <div>
-                    <h4>{name}</h4>
-                    <p>$ {price}</p>
-                    <p>{clearance}</p>
+                    <h4 style={{ color: 'black' }}>{name}</h4>
+                    <p style={{ color: 'black' }}>$ {price}
+                    {(onsale || clearance) && 
+                    <span
+                    style={{ color: 'black', background:'yellow', border:'solid 1px black', borderRadius:'5px', fontSize:'0.75em', width:'fit-content', padding:'3px' }}>
+                        Save $ {discount}
+                    </span>}
+                    </p>
                 </div>
             </Link>
     )
