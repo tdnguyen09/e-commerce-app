f109d1cf8ff487fcdd0799aedb94f7f7315b0452
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-08-29 17:32:16 +1000

    builing checkout component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { WebContext } from "../WebContext";
+import './Checkout.css'
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function Checkout({ user }) {
     const context = useContext(WebContext)
+    const [isOrderSubmit, setIsOrderSubmit] = useState(false);
+    const history = useHistory()
     let orderItems = context.cartItems
     let total = context.totalCost()
-    console.log(user)
-    console.log(context.cartItems)
 
     function handleClick() {
         const orderDetail = {
@@ -23,30 +25,53 @@ function Checkout({ user }) {
             },
             body: JSON.stringify(orderDetail),
         })
-        .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => {
+            if(res.ok) {
+                return res.json()}
+            })
+        .then(() => {
+            setIsOrderSubmit(true)
+            setTimeout(() => {
+                setIsOrderSubmit(false);
+                history.push('/')
+            }, 5000);
+        })  
     }
     return (
-        <div>
-            <div>
-                <h3>Review cart</h3>
-                {context.cartItems.map(item => (
-                    <div key={item.id}>
-                        <p>{item.name}</p>
-                        <p>{item.quantity}</p>
-                        <p>{item.price}</p>
-                        <p>Total: ${context.totalCost()}</p>
-                    </div>
-                ))}
+        <div id="checkout-page">
+            <div id={isOrderSubmit? "checkout-details-blur" : "checkout-details"}>
+            {/* <div id="checkout-details-blur" > */}
+                <div id="review-cart">
+                    <h3 style={{marginBottom:'20px'}}>Review cart</h3>
+                    {context.cartItems.map(item => (
+                        <div key={item.id} className="item-wrapper">
+                            <img src={item.image} alt={item.name} />
+                            <p style={{ fontWeight:'550'}}>{item.name}</p>
+                            <p>Qty: {item.quantity}</p>
+                            <p>${item.price}</p>
+                        </div>
+                    ))}
+                    <p style={{marginTop:'20px', fontWeight:'700'}}>Total: ${context.totalCost()}</p>
+                </div>
+                <div id="contact-confirm">
+                    <h3 style={{marginTop:'10px'}}>Contact Details</h3>
+                    <p style={{fontWeight:'600'}}>{user.firstname} {user.lastname}</p>
+                    <p style={{fontSize:'17px'}}>Email: {user.email}</p>
+                    <p style={{fontSize:'17px'}}>Address: {user.address}, {user.suburb}, {user.state} {user.postcode}</p>
+                    <p style={{fontSize:'17px'}}> Phone: {user.phonenumber}</p>
+                </div>
+                <button 
+                    onClick={() => handleClick()}
+                    style={{width:'150px', height:'40px', fontSize:'15px', backgroundColor:'blue', color:'white', fontWeight:'700', padding:'5px', cursor:'pointer'}}
+                >
+                    Submit Order
+                </button>
             </div>
-            <div>
-                <h3>Contact Details</h3>
-                <p>{user.firstname} {user.lastname}</p>
-                <p>{user.email}</p>
-                <p>Address:{user.address}, {user.suburb}, {user.state} {user.postcode}</p>
-                <p>Phone: {user.phonenumber}</p>
-            </div>
-            <button onClick={() => handleClick()}>Submit Order</button>
+            {isOrderSubmit && (
+                <div id="modal">
+                    <p>Order submitted successfully!</p>
+                </div>
+            )}
         </div>
     )
 }
