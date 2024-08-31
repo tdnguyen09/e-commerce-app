af2198870842aed8e607eb913d1e3b075172d305
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-09-01 00:08:24 +1000

    delete debug code

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Signup.css"
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import * as yup from "yup";
